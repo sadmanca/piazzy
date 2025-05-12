97d7a737b5bdb5aefa7c35fea790ceb74f33e1b6
Sadman Hossain <m.sadman.h@gmail.com>
2025-05-11 20:13:38 -0400

    fix: update port to allow for deploying on render

--- piazzy-web/app.py
+++ piazzy-web/app.py
@@ -71,4 +71,6 @@ def view_post(nid, cid):
     return render_template('post.html', post=post)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
